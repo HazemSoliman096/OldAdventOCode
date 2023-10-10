6b34e827cb8cf6fc125b1fd81b9758b6cbf1c5ca
HazemSoliman096 <hazemsoliman096@gmail.com>
2023-10-11 02:35:55 +0300

    puzzle one part two

--- 2015/puzzles/PuzzleOne.cs
+++ 2015/puzzles/PuzzleOne.cs
@@ -3,20 +3,25 @@ public class PuzzleOne
     public int getDate(string filepath)
     {
         int result = 0;
+        int position = 0;
+
         StreamReader streamReader = new StreamReader(filepath);
         string data = streamReader.ReadToEnd();
 
-        foreach (Char letter in data)
+        for (int i = 0; i < data.Length; i++)
         {
-            if (letter == '(')
+            if (data[i] == '(')
             {
                 result += 1;
             }
-            else if (letter == ')')
+            else if (data[i] == ')')
             {
                 result -= 1;
             }
+
+            position = position == 0 ? (result == -1 ? (i + 1) : 0) : position;
         }
+        Console.WriteLine(position);
         return result;
     }
 }
